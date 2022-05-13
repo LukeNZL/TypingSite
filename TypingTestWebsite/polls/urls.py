329797c550a1d615713c1004a9c59bcecce28e4b
LukeNZL <lukeyoobee@gmail.com>
2022-05-13 13:43:46 +1200

    Added templates, working with dynamic urls

--- TypingTestWebsite/polls/urls.py
+++ TypingTestWebsite/polls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
 
 ]
--- TypingTestWebsite/polls/views.py
+++ TypingTestWebsite/polls/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+
+rooms = [
+    {'id': 1, 'name':'Lets learn Python'},
+    {'id': 2, 'name': 'Deisgn with me'},
+    {'id': 3, 'name': 'front end devs'},
+
+]
 
 
 def index(request):
     return HttpResponse("Typing Test")
-
-
+def home(request):
+    context={'rooms':rooms}
+    return render(request,'polls/home.html',context)
+def room(request,pk):
+    room=None
+    for i in rooms:
+        if i['id']==int(pk):
+            room = i
+    context = {'room':room}
+    return render(request,'polls/room.html',context)

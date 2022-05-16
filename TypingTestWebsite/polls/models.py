92af687f70a796737d6611a0e619a313fb847224
LukeNZL <lukeyoobee@gmail.com>
2022-05-16 21:22:23 +1200

    admin django interface added

--- TypingTestWebsite/polls/models.py
+++ TypingTestWebsite/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date Published')
@@ -8,3 +9,25 @@ class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
     votes = models.IntegerField(default = 0 )
+
+class Room(models.Model):
+    #host =
+    #topic =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+    #participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    #user =
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
--- TypingTestWebsite/polls/views.py
+++ TypingTestWebsite/polls/views.py
@@ -3,24 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from.models import Room
 
-rooms = [
-    {'id': 1, 'name':'Lets learn Python'},
-    {'id': 2, 'name': 'Deisgn with me'},
-    {'id': 3, 'name': 'front end devs'},
-
-]
 
 
 def index(request):
     return HttpResponse("Typing Test")
 def home(request):
+    rooms=Room.objects.all()
     context={'rooms':rooms}
     return render(request,'polls/home.html',context)
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room = i
-    context = {'room':room}
+    room=Room.objects.get(id=pk)
+    context = {'room': room}
     return render(request,'polls/room.html',context)

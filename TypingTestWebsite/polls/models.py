065e7ce12a0f72f244bcc64cde268a0b74b2806d
LukeNZL <lukeyoobee@gmail.com>
2022-05-24 17:10:21 +1200

    added login, logout, register

--- TypingTestWebsite/polls/models.py
+++ TypingTestWebsite/polls/models.py
@@ -13,22 +13,28 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date Published')
 
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
-    choice_text=models.CharField(max_length=200)
-    votes = models.IntegerField(default = 0 )
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
 
 class Room(models.Model):
-    host = models.ForeignKey(User ,on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True,blank=True)
-    #participants =
+    description = models.TextField(null=True, blank=True)
+    # participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+
+    class Meta:
+        ordering = ['-updated','-created']
     def __str__(self):
         return self.name
+
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
